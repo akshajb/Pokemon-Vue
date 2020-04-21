ec7cfd0cb3311d5e771e6f5f41f89b5187ef62ba
akshajb <akshajb@gmail.com>
2020-04-22 01:40:25 +0530

    MVC structure for Backend

--- api-pokemon/src/app.js
+++ api-pokemon/src/app.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const router = require('./router')
 const port = process.env.PORT;
-
-
-var Pokedex = require('pokedex-promise-v2');
-var poke = new Pokedex();
 
 // CORS
 app.use(function(req, res, next) {
@@ -13,18 +10,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+    
 
-app.get('/region-list',(req,res)=>{
-    poke.getRegionsList()
-    .then(response=>{
-        res.send(response);
-    })
-    .catch(error=>{
-        res.send(error);
-    })
-})
-
-
+app.use('/', router);
 
 var server = app.listen(port, function () {
     console.log(`Listening on ${port}`) 
